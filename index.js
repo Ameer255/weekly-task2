36ea3e2b80bcbb09efa323d0a2989216c9708840
Ameer255 <64036480+Ameer255@users.noreply.github.com>
2020-05-18 11:28:19 +0500

    Add files via upload
    project files added

---
+++ index.js
@@ -0,0 +1,28 @@
+function check(){
+   
+    var x=document.getElementById("form");
+ var y=   x.getElementsByTagName("input");
+
+ for (i=0; i<y.length; i++)
+ {
+if (y[i].value=="")
+{
+    y[i].style.border="2px solid red";
+
+    var a=x.getElementsByClassName("warning");
+    
+        a[i].style="display: block;";
+      
+    
+}
+  
+}
+
+var b=document.getElementById("p1");
+var c=document.getElementById("p2");
+var z=document.getElementById("confirmation");
+if (b.value!=c.value){
+z.innerHTML="<p>Your Password Don't Match</p>";
+}
+
+}
